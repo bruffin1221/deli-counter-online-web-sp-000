7f64e997caf3c7a7c7d6b78afbc390214f2d17c9
Bruce Ruffin <bruffin1221@gmail.com>
2019-07-28 19:15:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,5 +9,6 @@ katz_deli.each.with_index(1) do |customer,num|
   line_num<<"#{num}. #{customer})"
   puts "The line is currently: #{line_num.join(" ")}"
 end
+puts "The line is currently: #{line_num.join(" ")}"
 end
 end

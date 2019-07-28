a30febbe1bb05973b99cde7e057f85018973bb33
Bruce Ruffin <bruffin1221@gmail.com>
2019-07-28 19:08:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli=[]
+
+def line(katz_deli)
+  line_num=[]
+if katz_deli.size==0
+  puts "The line is currently empty."
+else katz_deli.size>=1
+  katz_deli.each.with_index(1) do |customer,num|
+    line_num.<<"#{num}. #{customer}).join"
+    puts "The line is currently: #{line_num.join(" ")}"
+    end
+  end
+end

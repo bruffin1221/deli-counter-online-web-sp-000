729216225905eae6297347b059de305776bdf16b
Bruce Ruffin <bruffin1221@gmail.com>
2019-07-28 20:33:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,6 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   else katz_deli.length>=1
     puts "Currently serving #{katz_deli[0]}."
-    katz_deli.pop
+    katz_deli.shift
   end
 end

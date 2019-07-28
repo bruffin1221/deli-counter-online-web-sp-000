4ac171aadadfc7ebbd1e4856130b820d3e57c939
Bruce Ruffin <bruffin1221@gmail.com>
2019-07-28 19:23:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ puts "The line is currently empty."
 else katz_deli.size>=1
 katz_deli.each.with_index(1) do |customer,num|
   line_num<<"#{num}. #{customer}"
-  puts "The line is currently: #{line_num.join}"
 end
+  puts "The line is currently: #{line_num.join}"
 end
 end
